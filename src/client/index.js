2002accb9b065e8f5d72e70c08400aec91b90af5
Paritosh <paritosh@Paritoshs-MacBook-Pro.local>
2020-09-23 11:39:59 +0530

    initial setip

---
+++ src/client/index.js
@@ -0,0 +1,7 @@
+import { checkForName } from "./js/nameChecker";
+import { handleSubmit } from "./js/formHandler";
+
+console.log(checkForName);
+console.log(application_key);
+alert("I EXIST");
+console.log("CHANGE!!");
